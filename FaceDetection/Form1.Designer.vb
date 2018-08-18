ae2aded6b82f10edca55bef53da208dea74a033b
Ehabmagdyabbas <ihabmajdyabass@hotmail.com>
2018-08-18 13:20:58 +0200

    Add files via upload

---
+++ FaceDetection/Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FaceRecognation
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ComboBox_Devices = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.groupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.imageBox1 = New Emgu.CV.UI.ImageBox()
+        Me.imageBoxFrameGrabber = New Emgu.CV.UI.ImageBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.label5 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.label3 = New System.Windows.Forms.Label()
+        Me.groupBox2 = New System.Windows.Forms.GroupBox()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.groupBox1.SuspendLayout()
+        CType(Me.imageBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.imageBoxFrameGrabber, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.groupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ComboBox_Devices
+        '
+        Me.ComboBox_Devices.FormattingEnabled = True
+        Me.ComboBox_Devices.Location = New System.Drawing.Point(17, 167)
+        Me.ComboBox_Devices.Name = "ComboBox_Devices"
+        Me.ComboBox_Devices.Size = New System.Drawing.Size(178, 21)
+        Me.ComboBox_Devices.TabIndex = 18
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(24, 143)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(61, 21)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Stop"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(58, 170)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(106, 20)
+        Me.TextBox1.TabIndex = 13
+        Me.TextBox1.Text = "Ehab"
+        '
+        'groupBox1
+        '
+        Me.groupBox1.Controls.Add(Me.TextBox1)
+        Me.groupBox1.Controls.Add(Me.Button4)
+        Me.groupBox1.Controls.Add(Me.label1)
+        Me.groupBox1.Controls.Add(Me.imageBox1)
+        Me.groupBox1.Location = New System.Drawing.Point(532, 252)
+        Me.groupBox1.Name = "groupBox1"
+        Me.groupBox1.Size = New System.Drawing.Size(206, 242)
+        Me.groupBox1.TabIndex = 14
+        Me.groupBox1.TabStop = False
+        Me.groupBox1.Text = "Training: "
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(14, 208)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(160, 23)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "2. Add face"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(11, 173)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(41, 13)
+        Me.label1.TabIndex = 8
+        Me.label1.Text = "Name: "
+        '
+        'imageBox1
+        '
+        Me.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.imageBox1.Location = New System.Drawing.Point(11, 18)
+        Me.imageBox1.Name = "imageBox1"
+        Me.imageBox1.Size = New System.Drawing.Size(163, 134)
+        Me.imageBox1.TabIndex = 5
+        Me.imageBox1.TabStop = False
+        '
+        'imageBoxFrameGrabber
+        '
+        Me.imageBoxFrameGrabber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.imageBoxFrameGrabber.Location = New System.Drawing.Point(12, 4)
+        Me.imageBoxFrameGrabber.Name = "imageBoxFrameGrabber"
+        Me.imageBoxFrameGrabber.Size = New System.Drawing.Size(514, 490)
+        Me.imageBoxFrameGrabber.TabIndex = 13
+        Me.imageBoxFrameGrabber.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.Label4.ForeColor = System.Drawing.Color.Blue
+        Me.Label4.Location = New System.Drawing.Point(9, 59)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 19)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "Nobody"
+        '
+        'label5
+        '
+        Me.label5.AutoSize = True
+        Me.label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label5.ForeColor = System.Drawing.Color.Black
+        Me.label5.Location = New System.Drawing.Point(9, 23)
+        Me.label5.Name = "label5"
+        Me.label5.Size = New System.Drawing.Size(197, 15)
+        Me.label5.TabIndex = 17
+        Me.label5.Text = "Persons present in the scene:"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(16, 194)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(179, 37)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "1. Detect and recognize"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'label3
+        '
+        Me.label3.AutoSize = True
+        Me.label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label3.ForeColor = System.Drawing.Color.Red
+        Me.label3.Location = New System.Drawing.Point(163, 124)
+        Me.label3.Name = "label3"
+        Me.label3.Size = New System.Drawing.Size(16, 16)
+        Me.label3.TabIndex = 15
+        Me.label3.Text = "0"
+        '
+        'groupBox2
+        '
+        Me.groupBox2.Controls.Add(Me.ComboBox_Devices)
+        Me.groupBox2.Controls.Add(Me.Button1)
+        Me.groupBox2.Controls.Add(Me.Label4)
+        Me.groupBox2.Controls.Add(Me.label5)
+        Me.groupBox2.Controls.Add(Me.Button3)
+        Me.groupBox2.Controls.Add(Me.label3)
+        Me.groupBox2.Controls.Add(Me.label2)
+        Me.groupBox2.Location = New System.Drawing.Point(532, 4)
+        Me.groupBox2.Name = "groupBox2"
+        Me.groupBox2.Size = New System.Drawing.Size(208, 242)
+        Me.groupBox2.TabIndex = 15
+        Me.groupBox2.TabStop = False
+        Me.groupBox2.Text = "Results: "
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label2.Location = New System.Drawing.Point(10, 92)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(179, 15)
+        Me.label2.TabIndex = 14
+        Me.label2.Text = "Number of faces detected: "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(750, 502)
+        Me.Controls.Add(Me.groupBox1)
+        Me.Controls.Add(Me.imageBoxFrameGrabber)
+        Me.Controls.Add(Me.groupBox2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.groupBox1.ResumeLayout(False)
+        Me.groupBox1.PerformLayout()
+        CType(Me.imageBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.imageBoxFrameGrabber, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.groupBox2.ResumeLayout(False)
+        Me.groupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ComboBox_Devices As System.Windows.Forms.ComboBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Private WithEvents groupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Private WithEvents label1 As System.Windows.Forms.Label
+    Private WithEvents imageBox1 As Emgu.CV.UI.ImageBox
+    Private WithEvents imageBoxFrameGrabber As Emgu.CV.UI.ImageBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Private WithEvents label5 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Private WithEvents label3 As System.Windows.Forms.Label
+    Private WithEvents groupBox2 As System.Windows.Forms.GroupBox
+    Private WithEvents label2 As System.Windows.Forms.Label
+
+End Class
---
+++ FaceDetection/Form1.vb
@@ -0,0 +1,264 @@
+﻿Imports Emgu.CV
+Imports Emgu.CV.CvEnum
+Imports Emgu.CV.Structure
+Imports Emgu.CV.UI
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Diagnostics
+Imports System.Drawing
+Imports System.IO
+Imports System.Windows.Forms
+Public Class FaceRecognation
+    Inherits Form
+    Private currentFrame As Image(Of Bgr, Byte)
+
+
+    Public grabber As Emgu.CV.Capture
+
+    Private face As HaarCascade
+
+    Private eye As HaarCascade
+
+    Private mouth As HaarCascade
+
+    Private body As HaarCascade
+
+    Private bodyupper As HaarCascade
+
+    Private bodylower As HaarCascade
+
+
+
+    Private font As MCvFont = New MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.5, 0.5)
+
+    Private result As Image(Of Emgu.CV.Structure.Gray, Byte)
+
+    Private TrainedFace As Image(Of Emgu.CV.Structure.Gray, Byte)
+
+    Private gray As Image(Of Emgu.CV.Structure.Gray, Byte)
+
+    Private trainingImages As List(Of Image(Of Emgu.CV.Structure.Gray, Byte)) = New List(Of Image(Of Emgu.CV.Structure.Gray, Byte))()
+
+    Private labels As List(Of String) = New List(Of String)()
+
+    Private NamePersons As List(Of String) = New List(Of String)()
+
+    Private ContTrain As Integer
+
+    Private NumLabels As Integer
+
+    Private t As Integer
+
+    Private name As String
+
+    Private names As String
+
+    Public Sub New()
+        MyBase.New()
+        Me.InitializeComponent()
+        Me.face = New HaarCascade("haarcascade_frontalface_default.xml")
+        Me.eye = New HaarCascade("haarcascade_eye.xml")
+        Me.mouth = New HaarCascade("Mouth.xml")
+        Me.body = New HaarCascade("haarcascade_fullbody.xml")
+
+        Me.bodyupper = New HaarCascade("haarcascade_upperbody.xml")
+        Me.bodylower = New HaarCascade("haarcascade_lowerbody.xml")
+
+
+        Try
+            Dim Labelsinfo As String = File.ReadAllText(String.Concat(Application.StartupPath, "/TrainedFaces/TrainedLabels.txt"))
+            Dim Labels As String() = Labelsinfo.Split(New Char() {"%"c})
+            Me.NumLabels = Convert.ToInt16(Labels(0))
+            Me.ContTrain = Me.NumLabels
+            For tf As Integer = 1 To Me.NumLabels + 1
+                Dim LoadFaces As String = String.Concat("face", tf, ".bmp")
+                Me.trainingImages.Add(New Image(Of Emgu.CV.Structure.Gray, Byte)(String.Concat(Application.StartupPath, "/TrainedFaces/", LoadFaces)))
+                Me.labels.Add(Labels(tf))
+            Next
+
+        Catch exception As System.Exception
+            MessageBox.Show("Nothing in binary database, please add at least a face(Simply train the prototype with the Add Face Button).", "Triained faces load", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End Try
+
+    End Sub
+
+
+    Dim t0 As Type = Type.GetTypeFromProgID("SAPI.SpVoice")
+    Dim voice As Object = Activator.CreateInstance(t0)
+    Dim check As String
+    Public opencam As String
+    Sub speakname(ByVal name As String)
+        If check <> name Then
+
+            '  t0 = Type.GetTypeFromProgID("SAPI.SpVoice")
+            '  voice = Activator.CreateInstance(t0)
+
+            '  voice.Speak("how are you " & name)
+
+        End If
+        If check <> name And check = "Ehab" Then
+
+
+
+            voice.Speak("how are you " & name)
+
+        End If
+        check = name
+    End Sub
+
+    Public Sub FrameGrabber(ByVal sender As Object, ByVal e As EventArgs)
+        If opencam = "yes" Then
+            Me.label3.Text = "0"
+            Me.NamePersons.Add("")
+            Me.currentFrame = Me.grabber.QueryFrame().Resize(514, 478, INTER.CV_INTER_CUBIC)
+            '514, 478
+            Me.gray = Me.currentFrame.Convert(Of Gray, Byte)()
+            Dim facesDetected As MCvAvgComp()() = Me.gray.DetectHaarCascade(Me.face, 1.2, 10, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, New Size(20, 20))
+            Dim array As MCvAvgComp() = facesDetected(0)
+            For i As Integer = 0 To array.Length - 1
+                Dim f As MCvAvgComp = array(i)
+                'Detecet Face 
+                Me.t += 1
+                Me.result = Me.currentFrame.Copy(f.rect).Convert(Of Gray, Byte)().Resize(100, 100, INTER.CV_INTER_CUBIC)
+                Me.currentFrame.Draw(f.rect, New Bgr(Color.Red), 2)
+                : If Me.trainingImages.ToArray().Length <> 0 Then
+                    : Dim termCrit As MCvTermCriteria = New MCvTermCriteria(Me.ContTrain, 0.001)
+                    : Dim recognizer As EigenObjectRecognizer = New EigenObjectRecognizer(Me.trainingImages.ToArray(), Me.labels.ToArray(), 3000.0, termCrit)
+                    : Me.name = recognizer.Recognize(Me.result)
+                    : Dim arg_192_0 As Image(Of Bgr, Byte) = Me.currentFrame
+                    : Dim arg_192_1 As String = Me.name
+                    : Dim rect As Rectangle = f.rect
+                    : Dim arg_183_0 As Integer = rect.X - 2
+                    : Dim rect2 As Rectangle = f.rect
+                    : arg_192_0.Draw(arg_192_1, Me.font, New Point(arg_183_0, rect2.Y - 2), New Bgr(Color.Orange))
+                    : arg_192_0.Draw(Today, Me.font, New Point(arg_183_0, rect2.Y + 20), New Bgr(Color.Orange))
+                    : arg_192_0.Draw(TimeOfDay, Me.font, New Point(arg_183_0 + 150, rect2.Y + 20), New Bgr(Color.Orange))
+                    : speakname(arg_192_1)
+
+                    Dim bdyupr As MCvAvgComp()() = Me.gray.DetectHaarCascade(Me.bodyupper, 1.2, 1, CvEnum.HAAR_DETECTION_TYPE.FIND_BIGGEST_OBJECT, New Size(50, 50))
+
+                    For Each bdyup As MCvAvgComp In bdyupr(0)
+                        '  Me.currentFrame.Draw(bdyup.rect, New Bgr(Color.Blue), 3)
+                    Next
+
+                    Dim bdylow As MCvAvgComp()() = Me.gray.DetectHaarCascade(Me.bodylower, 1.2, 1, CvEnum.HAAR_DETECTION_TYPE.FIND_BIGGEST_OBJECT, New Size(50, 50))
+
+                    For Each bdylw As MCvAvgComp In bdylow(0)
+                        '     Me.currentFrame.Draw(bdylw.rect, New Bgr(Color.Blue), 3)
+                    Next
+
+                    Dim bodys As MCvAvgComp()() = Me.gray.DetectHaarCascade(Me.body, 1.2, 1, CvEnum.HAAR_DETECTION_TYPE.FIND_BIGGEST_OBJECT, New Size(50, 50))
+
+                    For Each body As MCvAvgComp In bodys(0)
+                        '   Me.currentFrame.Draw(body.rect, New Bgr(Color.Blue), 3)
+                    Next
+                    'Detecet eyes
+                    : For Each f0 As MCvAvgComp() In facesDetected
+                        'Set the region of interest on the faces
+                        Me.gray.ROI = f.rect
+                        : Dim eyesDetected = Me.gray.DetectHaarCascade(Me.eye, 1.3, 12, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, New Size(20, 20))
+                        : Me.gray.ROI = Rectangle.Empty
+                        : For Each m In eyesDetected(0)
+                            Dim eyeRect As Rectangle = m.rect
+                            eyeRect.Offset(f.rect.X, f.rect.Y)
+                            : Me.currentFrame.Draw(eyeRect, New Bgr(Color.Green), 1)
+
+                            : Next
+
+                        : Next
+                    'Detecet mouth
+                    : For Each f0 As MCvAvgComp() In facesDetected
+                        'Set the region of interest on the faces
+                        : Me.gray.ROI = f.rect
+                        : Dim mouthsDetected = Me.gray.DetectHaarCascade(Me.mouth, 1.3, 12, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, New Size(20, 20))
+                        : Me.gray.ROI = Rectangle.Empty
+                        : For Each m In mouthsDetected(0)
+                            : Dim mouthRect As Rectangle = m.rect
+                            : mouthRect.Offset(f.rect.X, f.rect.Y)
+                            ' Me.currentFrame.Draw(mouthRect, New Bgr(Color.Yellow), 1)
+                            : Next
+
+                        : Next
+                    : End If
+                : Me.NamePersons(Me.t - 1) = Me.name
+                : Me.NamePersons.Add("")
+                : Me.label3.Text = facesDetected(0).Length.ToString()
+                : Next
+            Me.t = 0
+            For nnn As Integer = 0 To facesDetected(0).Length - 1
+                Me.names = Me.names + Me.NamePersons(nnn) + ", "
+            Next
+            Me.imageBoxFrameGrabber.Image = Me.currentFrame
+            ' sir.imageBoxFrameGrabber.Image = Me.currentFrame
+            Me.Label4.Text = Me.names
+            Me.names = ""
+            Me.NamePersons.Clear()
+        ElseIf opencam <> "yes" Then
+            Me.grabber.Dispose()
+        ElseIf opencam = "body" Then
+
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Me.grabber = New Emgu.CV.Capture(ComboBox_Devices.SelectedIndex)
+        Me.grabber.QueryFrame()
+        opencam = "yes"
+        AddHandler Application.Idle, New EventHandler(AddressOf FrameGrabber)
+        Me.Button3.Enabled = False
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        '   514, 478
+        Try
+            Me.ContTrain = Me.ContTrain + 1
+            Me.gray = Me.grabber.QueryGrayFrame().Resize(320, 240, INTER.CV_INTER_CUBIC)
+            Dim facesDetected As MCvAvgComp()() = Me.gray.DetectHaarCascade(Me.face, 1.2, 10, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, New System.Drawing.Size(20, 20))
+            Dim mCvAvgCompArray As MCvAvgComp() = facesDetected(0)
+            Dim num As Integer = 0
+            If (num < CInt(mCvAvgCompArray.Length)) Then
+                Dim f As MCvAvgComp = mCvAvgCompArray(num)
+                Me.TrainedFace = Me.currentFrame.Copy(f.rect).Convert(Of Emgu.CV.Structure.Gray, Byte)()
+            End If
+            Me.TrainedFace = Me.result.Resize(100, 100, INTER.CV_INTER_CUBIC)
+            Me.trainingImages.Add(Me.TrainedFace)
+            Me.labels.Add(Me.TextBox1.Text)
+            Me.imageBox1.Image = Me.TrainedFace
+            Dim str As String = String.Concat(Application.StartupPath, "/TrainedFaces/TrainedLabels.txt")
+            Dim length As Integer = CInt(Me.trainingImages.ToArray().Length)
+            File.WriteAllText(str, String.Concat(length.ToString(), "%"))
+            Dim i As Integer = 1
+            Do
+                Dim array As Image(Of Emgu.CV.Structure.Gray, Byte) = Me.trainingImages.ToArray()(i - 1)
+                Dim startupPath() As Object = {Application.StartupPath, "/TrainedFaces/face", i, ".bmp"}
+                DirectCast(array, CvArray(Of Byte)).Save(String.Concat(startupPath))
+                File.AppendAllText(String.Concat(Application.StartupPath, "/TrainedFaces/TrainedLabels.txt"), String.Concat(Me.labels.ToArray()(i - 1), "%"))
+                i = i + 1
+            Loop While i < CInt(Me.trainingImages.ToArray().Length) + 1
+            MessageBox.Show(String.Concat(Me.TextBox1.Text, "´s face detected and added :)"), "Training OK", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+        Catch
+            MessageBox.Show("Enable the face detection first", "Training Fail", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        On Error Resume Next
+        opencam = "no"
+        Me.grabber.Dispose()
+        Me.Button3.Enabled = True
+
+    End Sub
+
+    Private Sub FaceRecognation_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        opencam = "no"
+        Me.grabber.Dispose()
+        Me.Button3.Enabled = True
+    End Sub
+    Public WithEvents cam As New DSCamCapture
+    Private Sub FaceRecognation_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ComboBox_Devices.Items.AddRange(cam.GetCaptureDevices)
+        If ComboBox_Devices.Items.Count > 0 Then ComboBox_Devices.SelectedIndex = 0
+
+    End Sub
+End Class
